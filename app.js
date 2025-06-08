2164e6f100d534ee03965e57a584307b8798020b
shige100 <hospital.matsuo.s2022@gmail.com>
2025-06-08 19:45:50 +0900

    add dog api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
+var dogRouter = require('./routes/dog');
 
 var app = express();
 
@@ -26,7 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
-app.use('/notes', catRouter);
+app.use('/cat', catRouter);
+app.use('/dog', dogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
